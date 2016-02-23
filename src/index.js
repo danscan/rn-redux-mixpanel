691a5cc38c55d5c628a570add17d0ed9b57de591
Dan Scanlon <dscanlonpa@gmail.com>
2016-02-23 15:36:48 -0500

    Fixed unexpected token

--- src/index.js
+++ src/index.js
@@ -5,39 +5,41 @@ export default function mixpanel({
   token,
   selectDistinctId = () => null,
   selectUserProfileData = () => null,
-  selectEventName = (action) => action.type, 
+  selectEventName = (action) => action.type,
   selectProperties = () => null,
   ignoreAction = (action) => false,
-}) => store => next => action => {
-  // Don't track falsy actions or actions that should be ignored
-  if (!action.type || ignoreAction(action)) {
-    return next(action)
-  }
+}) {
+  return store => next => action => {
+    // Don't track falsy actions or actions that should be ignored
+    if (!action.type || ignoreAction(action)) {
+      return next(action)
+    }
 
-  // Get store state; select distinct id for action & state
-  const state = store.getState()
-  const distinctId = selectDistinctId(action, state)
-  const eventName = selectEventName(action, state)
-  const properties = selectProperties(action, state)
+    // Get store state; select distinct id for action & state
+    const state = store.getState()
+    const distinctId = selectDistinctId(action, state)
+    const eventName = selectEventName(action, state)
+    const properties = selectProperties(action, state)
 
-  // Track action event with Mixpanel
-  trackEvent({
-    token,
-    distinctId,
-    eventName: eventName,
-    eventData: properties
-  })
-
-  // Select user profile data for action; if it selects truthy data,
-  // update user profile on Mixpanel
-  const userProfileData = selectUserProfileData(action, state)
-  if (userProfileData) {
-    updateUserProfile({
+    // Track action event with Mixpanel
+    trackEvent({
       token,
       distinctId,
-      userProfileData,
+      eventName: eventName,
+      eventData: properties
     })
-  }
 
-  return next(action)
+    // Select user profile data for action; if it selects truthy data,
+    // update user profile on Mixpanel
+    const userProfileData = selectUserProfileData(action, state)
+    if (userProfileData) {
+      updateUserProfile({
+        token,
+        distinctId,
+        userProfileData,
+      })
+    }
+
+    return next(action)
+  }
 }
